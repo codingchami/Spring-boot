2cbd21b2ea4ae84ea05e8f6449b50e09976b5924
codingchami <cschamithsandeepa@gmail.com>
2024-11-03 12:25:23 +0530

    create Controller package and TestController class

--- pos/pos/src/main/java/com/iitpos/pos/controller/TestController.java
+++ pos/pos/src/main/java/com/iitpos/pos/controller/TestController.java
@@ -1,4 +1,15 @@
 package com.iitpos.pos.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/test")
+
 public class TestController {
+    public String getMyText(){
+        return "Hello World!..";
+    }
 }
