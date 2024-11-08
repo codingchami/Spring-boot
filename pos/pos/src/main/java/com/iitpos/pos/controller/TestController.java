512c02c95c822e50b70cab4c0f4b619d88f60e2e
codingchami <cschamithsandeepa@gmail.com>
2024-11-08 14:58:59 +0530

    how to works end points through postman

--- pos/pos/src/main/java/com/iitpos/pos/controller/TestController.java
+++ pos/pos/src/main/java/com/iitpos/pos/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.iitpos.pos.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/test")
 
 public class TestController {
+
+    //gives the end points for Mappings
+    @GetMapping(path ="get-text" )
     public String getMyText(){
         return "Hello World!..";
+    }
+    @GetMapping("get-name")
+    public String getMyName(){
+        return "Kamal";
     }
 }
